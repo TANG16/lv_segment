4392de06b911a07db8186bc43d7a43ffa421f5d4
Mattias Nilsson <mattis.o.a.nilsson@gmail.com>
2017-10-04 14:39:59 +0200

    Finished remapToPolarCoordinates.m and corresponding tests, it works very nicely. Removed the data folder. Also downloaded intersections.m from matlab central.

--- drawLV.m
+++ drawLV.m
@@ -1,16 +1,15 @@
-function h = drawLV(im, endoContour, epiContour, plotTitle)
+function drawLV(im, endoContour, epiContour, plotTitle)
 
-endocolor = 'r-';
-epicolor = 'g-';
+endoColor = 'r-';
+epiColor = 'g-';
 [centerX, centerY] = findLVCenter(endoContour(:,:));
 
-h = figure(10);
 imagesc(im); colormap gray; axis image; colorbar; caxis([0 0.6]);
 hold on
 plot(endoContour(:,2), ...
-    endoContour(:,1), endocolor);
+    endoContour(:,1), endoColor);
 plot(epiContour(:,2), ...
-    epiContour(:,1), epicolor);
+    epiContour(:,1), epiColor);
 plot(centerY, centerX, 'Marker', '*', 'Color', [0 0 1], 'MarkerSize', 10);
 title(plotTitle);
 drawnow;
--- tests.m
+++ tests.m
@@ -1,7 +1,5 @@
 %% Draw images with segmentation and centerpoint.
 load lv_contours.mat
-endocolor = 'r-';
-epicolor = 'g-';
 
 for nPat = 4:length(outdata)
     IM = outdata(nPat).DiaIm;
@@ -26,36 +24,71 @@ for nPat = 4:length(outdata)
 end
 
 %% Test image polar remapping
-
-imNo = 25;
+clear all
+close all
 load lv_contours.mat
-IM = cat(3,Outdata.SYSIM, Outdata.DIAIM);
-MASK = cat(3,Outdata.SYSMASK, Outdata.DIAMASK);
-im = IM(:,:,imNo);
-mask = MASK(:,:,imNo);
+endoColor = 'r-';
+epiColor = 'g-';
 
-props = regionprops(mask,'Centroid');
-centerX = props.Centroid(1);
-centerY = props.Centroid(2);
-nAngle = 96;
-nRadius = 56;
-
-polarIm = imToPolarCoordinates(im, [centerX, centerY], nRadius, nAngle);
-interpolationMode = cell(1,3);
-interpolationMode{1,1} = 'nearest';
-interpolationMode{1,2} = 'bilinear';
-interpolationMode{1,3} = 'bicubic';
-for i = 1:3
+for nPat = 1 %:length(outdata)
     
-    polarIm = imToPolarCoordinates(im, [centerX, centerY], nRadius, nAngle,...
-        interpolationMode{i});
-    figure;
-    suptitle(['Polar remapping using ' interpolationMode{i} ' interpolation']);
-    subplot(2,1,1); imagesc(im); colormap gray; axis image;
-    hold on
-    plot(centerX, centerY, 'Marker', '*', 'Color', [1 0 0], 'MarkerSize', 12);
-    hold off
-    title('Input image');
-    subplot(2,1,2); imagesc(polarIm); colormap gray; axis image;
-    title('Polar Input image'); colorbar; caxis([0 0.6]);
+    IM = outdata(nPat).DiaIm;
+    for nIm = 1:size(IM,3)
+        [centerX, centerY] = findLVCenter(outdata(nPat).DiaEpi(:,:,nIm));
+        
+        %         h1 = figure(10);
+        figure;
+        drawLV(IM(:,:,nIm), ...
+            outdata(nPat).DiaEndo(:,:,nIm), ...
+            outdata(nPat).DiaEpi(:,:,nIm), ...
+            ['Diastolic LV for patient ' num2str(nPat) ', slice ' num2str(nIm)]);
+%         movegui(h1,'northwest');
+        
+        [polarIm, polarContour] = remapToPolarCoordinates(IM(:,:,nIm), ...
+            outdata(nPat).DiaEndo(:,:,nIm), outdata(nPat).DiaEpi(:,:,nIm), ...
+            centerX, centerY);
+        
+        figure;
+        %         h2 = figure(11);
+        imagesc(polarIm); hold on;
+        colormap gray; axis image; title('Polar image with segmentation');
+        colorbar; caxis([0 0.6]);
+        plot(1:size(polarIm,2), polarContour(:,1), endoColor);
+        plot(1:size(polarIm,2), polarContour(:,2), epiColor);
+        title(['Polar diastolic LV for patient ' num2str(nPat) ', slice ' num2str(nIm)]);
+%         movegui(h2,'southwest');
+        drawnow;
+%         disp('waiting');
+        %         pause;
+    end
+    %     close(h1);
+    %     close(h2);
+    
+    IM = outdata(nPat).SysIm;
+    for nIm = 1 %:length(outdata)
+        [centerX, centerY] = findLVCenter(outdata(nPat).DiaEpi(:,:,nIm));
+        %         h3 = figure(12);
+        figure;
+        drawLV(outdata(nPat).SysIm(:,:,nIm), ...
+            outdata(nPat).SysEndo(:,:,nIm), ...
+            outdata(nPat).SysEpi(:,:,nIm), ...
+            ['Systolic LV for patient ' num2str(nPat) ', slice' num2str(nIm)]);
+%         movegui(h3, 'northeast');
+        
+        %         h4 = figure(13);
+        figure;
+        imagesc(polarIm); hold on;
+        colormap gray; axis image; title('Polar image with segmentation');
+        colorbar; caxis([0 0.6]);
+        % Draw contour.
+        plot(1:size(polarIm,2), polarContour(:,1), endoColor);
+        plot(1:size(polarIm,2), polarContour(:,2), epiColor);
+        title(['Polar systolic LV for patient ' num2str(nPat) ', slice ' num2str(nIm)]);
+%         movegui(h4,'southeast');
+        drawnow;
+%         disp('waiting');
+        %         pause;
+    end
+    %     close(h3);
+    %     close(h4);
 end
